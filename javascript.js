204c861cd1c049ba8b9bf40d98c2a05270b524ae
abdul-iman <abdulle.a.iman@gmail.com>
2024-12-05 14:22:43 +0000

    task 2 completed

--- javascript.js
+++ javascript.js
@@ -1,8 +1,7 @@
 let moves = ["rock", "paper", "scissors"]
-
 let playerMove = "rock";
 let computerMove = "scissors";
-
+/*
 if(playerMove === computerMove) {
 console.log("It's a draw!");
 }
@@ -26,11 +25,42 @@ else if(playerMove === "paper" && computerMove === "scissors") {
 }
 else {
     console.log("Invalid input. Please choose either rock, scissors or paper.")
+}*/
 
-}
-
-
-
+function getWinner(playerMove, computerMove) {
+    // code goes here...
+    
+    if(playerMove === computerMove) {
+        return `0`;
+        }
+        else if(playerMove === "rock" && computerMove === "scissors") {
+            return `1`;
+        }
+        else if(playerMove === "scissors" && computerMove === "paper") {
+            return `1`;
+        }
+        else if(playerMove === "paper" && computerMove === "rock") {
+            return `1`;
+        }
+        else if(playerMove === "rock" && computerMove === "paper") {
+            return `-1`;
+        }
+        else if(playerMove === "scissors" && computerMove === "rock") {
+            return `-1`;
+        }
+        else if(playerMove === "paper" && computerMove === "scissors") {
+            return `-1`;
+        }
+        else {
+            console.log("Invalid input. Please choose either rock, scissors or paper.")   
+        }
+        
+    
+  }
+let result = getWinner("rock", "paper");
+// Task for function 4
+// const random = Math.floor(Math.random() * moves.length); 
+//console.log (random, moves[random])
 /*
     if (hour < 18) {
         greeting = "Good day";
