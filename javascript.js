8b4771804ff4145903fe0d8e79f2cb87b9fa4331
Samantha <178785253+fenrissa92@users.noreply.github.com>
2024-12-06 13:27:02 +0000

    attempts for MVP 5

--- javascript.js
+++ javascript.js
@@ -1,4 +1,100 @@
+let moves = ["rock", "paper", "scissors"];
+let gamesPlayed = 0;
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
+function getWinner(playerMove, computerMove) {
+    // Validation of player input
+    if (!moves.includes(playerMove)) {
+        return `Invalid input. Please choose either rock, scissors or paper.`
+    }
+
+    if(playerMove === computerMove) {
+        draws++;
+        return 0;
+    }
+    else if(playerMove === "rock" && computerMove === "scissors") {
+        wins++;
+        return 1;
+    }
+    else if(playerMove === "scissors" && computerMove === "paper") {
+        wins++;
+        return 1;
+    }
+    else if(playerMove === "paper" && computerMove === "rock") {
+        wins++;
+        return 1;
+    }
+    else if(playerMove === "rock" && computerMove === "paper") {
+        losses++;
+        return -1;
+    }
+    else if(playerMove === "scissors" && computerMove === "rock") {
+        looses++
+        return -1;
+    }
+    else if(playerMove === "paper" && computerMove === "scissors") {
+        looses++
+        return -1;
+        }
+    }
+
+function playGame() {
+    do {
+        let playerMove = prompt("Type in either rock, paper, or scissors in lowercase!");
+        let computerMove = moves[Math.floor(Math.random() * moves.length)];
+
+        let result = getWinner(playerMove, computerMove);
+    }
+
+        gamesPlayed++;
+
+        
+        let outcomeMessage = alert(`Games Played: ${gamesPlayed}\nWins: ${wins}\nLosses: ${losses}\nDraws: ${draws}`)";
+        
+
+     while (confirm("Do you want to play again?"));
+
+    // Display final stats
+    alert(`Games Played: ${gamesPlayed}\nWins: ${wins}\nLosses: ${losses}\nDraws: ${draws}`);
+}
+
+// Start the game
+playGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let playGame = true;
+let gamesPlayed = 0;
+let wins = 0;
+let losses = 0;
+let draws = 0;
 
 while (playGame) {
 let moves = ["rock", "paper", "scissors"]
@@ -12,39 +108,54 @@ function getWinner(playerMove, computerMove) {
     }
 
     if(playerMove === computerMove) {
-        return `0`;
+        draws++;
+        return 0;
         }
         else if(playerMove === "rock" && computerMove === "scissors") {
-            return `1`;
+            wins++;
+            return 1;
         }
         else if(playerMove === "scissors" && computerMove === "paper") {
-            return `1`;
+            wins++;
+            return 1;
         }
         else if(playerMove === "paper" && computerMove === "rock") {
-            return `1`;
+            wins++;
+            return 1;
         }
         else if(playerMove === "rock" && computerMove === "paper") {
-            return `-1`;
+            losses++;
+            return -1;
         }
         else if(playerMove === "scissors" && computerMove === "rock") {
-            return `-1`;
+            looses++
+            return -1;
         }
         else if(playerMove === "paper" && computerMove === "scissors") {
-            return `-1`;
+            looses++
+            return -1;
         }
     }
 
-    let result = alert(getWinner(playerMove, computerMove))
+    let result = alert((getWinner(playerMove, computerMove)), showStats)
 
-    playGame = confirm ("Do you want to play again?")
+    playGame = confirm("Do you want to play again?")
 }
+
+function showStats() {
+    alert("Games Played: ${gamesPlayed}\nWins: ${wins}\nLosses: ${losses}\nDraws: ${draws}");
+}
+
+
+*/
+
+
+
+
 
 /*if (playGame) true; {
    if  ("do you what to play again?");
 }
-
-
-
 
 //let result = getWinner("rock", "paper");
 
@@ -73,11 +184,6 @@ else if(playerMove === "paper" && computerMove === "scissors") {
 else {
     console.log("Invalid input. Please choose either rock, scissors or paper.")
 }*/
-
-
-
-
-
 
 /* Task for function 4
 const random = Math.floor(Math.random() * moves.length); 
